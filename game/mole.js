d534d46841592e82a99b600776b72a083368f1e7
Anders Nissen <andnis@gmail.com>
2013-10-26 14:44:58 +0200

    Update

--- game/mole.js
+++ game/mole.js
@@ -13,11 +13,18 @@ var MOVE_RIGHT = 2;
 var moveDirection = MOVE_UP;
 var tileGroup;
 
+var emitter;
+
 function preload() {
-  game.load.image('background','assets/misc/starfield.png');
-  game.load.spritesheet('item', 'assets/buttons/number-buttons-90x90.png', 90, 90);
+  game.load.image('background','assets/games/tanks/earth.png');
+  game.load.spritesheet('tile', 'assets/buttons/number-buttons-90x90.png', 90, 90);
+  // game.load.image('sand', 'assets/platformer/sandCenter_rounded.png');
+  // game.load.image('dirt', 'assets/platformer/snowCenter_rounded.png');
+  // game.load.image('stone', 'assets/platformer/stoneWall.png');
   game.load.image('player', 'assets/sprites/shinyball.png');
+  // game.load.image('magma','assets/games/breakout/brick2.png');
   game.load.image('magma','assets/games/breakout/brick2.png');
+  game.load.image('diamond', 'assets/games/starstruck/star.png');
 }
 
 function create() {
@@ -31,14 +38,27 @@ function create() {
 
   tileGroup = game.add.group();
 
+  // var tileImages = ['sand', 'dirt', 'stone'];
+  // tileImages[tileType]
+
   for (var y = 0; y < 72; y++) {
-    for (var x = 0; x < 6; x++) {
-      var tileType = rnd.integerInRange(0,6);
-      var sprite = tileGroup.create(90 * x, game.world.height - game.height - 90 * y, 'item', tileType);
-      sprite.health = tileType * tileType * 10;
+    for (var x = 0; x < 5; x++) {
+      var tileType = rnd.integerInRange(1,4);
+      var sprite = tileGroup.create(90 * x, game.world.height - game.height - 90 * y, 'tile', (tileType-1));
+      sprite.type = tileType;
+      sprite.health = 50 + tileType * tileType * 10;
       sprite.body.immovable = true;
     }
   }
+
+  emitter = game.add.emitter(0, 0, 1000);
+  emitter.minParticleSpeed.setTo(-200, 0);
+  emitter.maxParticleSpeed.setTo(200, 200);
+  emitter.gravity = 8;
+  emitter.bounce.setTo(0.5, 0.5);
+  emitter.particleDrag.x = 10;
+  emitter.angularDrag = 30;
+  emitter.makeParticles('diamond');
 
   player = game.add.sprite(game.width / 2, game.world.height - game.height / 2, 'player');
 
@@ -61,16 +81,27 @@ function update() {
 
   var collision = game.physics.collide(player, tileGroup, collisionHandler, null, this);
   player.scale.y = (collision ? 0.8 : 1);
+
+  game.physics.collide(emitter, tileGroup);
 }
 
 function collisionHandler (player, tile) {
   if (tile !== lastTile)
-    damage += 4;
-  if (!lastTile || tile.frame !== lastTile.frame)
-    damage = 4;
+    damage += 3;
+  if (!lastTile || tile.type !== lastTile.type)
+    damage = 3;
   lastTile = tile;
+  tile.alpha = tile.health / (50 + tile.type * tile.type * 10);
   tile.damage(damage);
   movePlayer();
+  particleBurst();
+}
+
+function particleBurst() {
+  emitter.x = player.x + player.width / 2;
+  emitter.y = player.y;
+
+  emitter.start(true, 1000, null, damage / 10);
 }
 
 function movePlayer() {
